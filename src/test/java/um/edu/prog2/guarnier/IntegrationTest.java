76d6c72287fa84908ed8bc4e957ddf38c2b37bf8
Facundo-Guarnier <81172384+Facundo-Guarnier@users.noreply.github.com>
2023-11-02 17:14:29 -0300

    Initial version of prog2 generated by generator-jhipster@7.9.3

---
+++ src/test/java/um/edu/prog2/guarnier/IntegrationTest.java
@@ -0,0 +1,22 @@
+package um.edu.prog2.guarnier;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import um.edu.prog2.guarnier.Prog2App;
+import um.edu.prog2.guarnier.config.AsyncSyncConfiguration;
+import um.edu.prog2.guarnier.config.EmbeddedSQL;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { Prog2App.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
